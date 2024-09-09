2486c71b57556b205c5518c0856b3ec9d154bcad
Ajibola Sorunke <sas4succex@gmail.com>
2024-09-09 22:37:11 +0100

    Displayin platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,43 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    //the type here is called index signature. Each key is map to iconType defined in react-icons
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    nintendo: SiNintendo,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+  return (
+    <>
+      <HStack marginY={1}>
+        {platforms.map((platform) => (
+          <Icon as={iconMap[platform.slug]} color="gray.500" />
+        ))}
+      </HStack>
+    </>
+  );
+};
+
+export default PlatformIconList;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,16 @@ import { useEffect, useState } from "react";
 import apiClient, { CanceledError } from "../services/apiClient";
 
 
+export interface Platform{
+    id: number;
+    name: string;
+    slug: string;
+}
 export interface Game {
     id: number;
     name: string;
-    background_image: string
+    background_image: string;
+    parent_platforms: { platform: Platform }[];
   }
   
   interface GamesResponse {
