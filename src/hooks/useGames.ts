d58e86cfd6a2d04169f3efef1bbed1ee648d5d46
Ajibola Sorunke <sas4succex@gmail.com>
2024-09-09 13:12:02 +0100

    Building game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import apiClient, { CanceledError } from "../services/apiClient";
 
 
-interface Game {
+export interface Game {
     id: number;
     name: string;
+    background_image: string
   }
   
   interface GamesResponse {
