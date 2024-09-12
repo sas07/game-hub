f0fa6d7d87547b76fa8ae7ddbdcd7bf767e51dfa
Ajibola Sorunke <sas4succex@gmail.com>
2024-09-12 15:50:48 +0100

    Refactor: extract a query object

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,5 @@
+import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
 
 
 export interface Platform{
@@ -15,10 +15,10 @@ export interface Game {
     metacritic: number
   }
   
-const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => useData<Game>("/games", {
+const useGames = (gameQuery: GameQuery) => useData<Game>("/games", {
   params: {
-  genres: selectedGenre?.id, 
-  platforms: selectedPlatform?.id 
+  genres: gameQuery.genre?.id, 
+  platforms: gameQuery.platform?.id 
 }}, 
-  [selectedGenre?.id, selectedPlatform?.id]);// these are the dependencies used in the useEffect hook
+  [gameQuery]);// these are the dependencies used in the useEffect hook
 export default useGames
