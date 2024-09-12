bf1f897dd10d06a2e0ee682b7ddda776a7595b6e
Ajibola Sorunke <sas4succex@gmail.com>
2024-09-12 14:45:17 +0100

    Filter games by platform

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -15,5 +15,10 @@ export interface Game {
     metacritic: number
   }
   
-const useGames = (selectedGenre: Genre | null) => useData<Game>("/games", {params: {genres:selectedGenre?.id }}, [selectedGenre?.id]);
+const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => useData<Game>("/games", {
+  params: {
+  genres: selectedGenre?.id, 
+  platforms: selectedPlatform?.id 
+}}, 
+  [selectedGenre?.id, selectedPlatform?.id]);// these are the dependencies used in the useEffect hook
 export default useGames
